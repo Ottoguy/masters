07fb7fd7090c32da30374c0a1228b109f58bc367
Ottoguy <otto.palmlof@gmail.com>
2024-01-15 13:48:39 +0100

    gitignore

--- model/load.py
+++ model/load.py
@@ -5,8 +5,19 @@ print("Data load initiated, expect data load time of up to 3 minutes.")
 timeStart = pd.Timestamp.now()
 # sheet_name = None: read all sheets
 data = pd.read_excel(
-    'data/ConsumptionExportFor2021AndJanary2022.xlsx', sheet_name=None, nrows=float('inf'))
+    'data/ConsumptionExportFor2021AndJanary2022.xlsx', sheet_name="2021-02")
 timeEnd = pd.Timestamp.now()
 print("Data load time: ", timeEnd - timeStart)
 print("Data load completed")
-print(data)
+print("Data type: ", type(data))
+if isinstance(data, dict):
+    print("Data keys: ", data.keys())
+
+elif isinstance(data, pd.DataFrame):
+    print("Data shape: ", data.shape)
+    print("Data columns: ", data.columns)
+    print(data)
+
+else:
+    print("Data type not recognized")
+    print("Data: ", data)
