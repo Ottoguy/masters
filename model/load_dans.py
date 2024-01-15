d850dcaf9758bfc4b85d04fe6019abe505b87338
Ottoguy <otto.palmlof@gmail.com>
2024-01-15 15:51:37 +0100

    loading dans

--- model/load_dans.py
+++ model/load_dans.py
@@ -8,7 +8,8 @@ folder_path = 'data/Dansmästaren/Charger_Data/'
 csv_files = [f for f in os.listdir(folder_path) if f.endswith('.csv')]
 
 # Initialize a list to store data from all CSV files
-all_data = []
+all_data = [["filename", "timestamp", "availability",
+            "a_fas1", "a_fas2", "a_fas3", "b_fas1", "b_fas2", "b_fas3", "unknown", "empty", "connection", "id"]]
 
 # Loop through each CSV file and read its content
 for csv_file in csv_files:
@@ -17,13 +18,20 @@ for csv_file in csv_files:
     with open(file_path, 'r', newline='') as file:
         csv_reader = csv.reader(file)
 
-        # Skip the header if needed
-        # next(csv_reader)
-
-        # Read the data and append it to the list
+        # Read the data
         file_data = list(csv_reader)
-        all_data.extend(file_data)
 
-# Now 'all_data' contains the data from all CSV files
+        # Add the filename as the first column in each row
+        file_data_with_filename = [[csv_file] + row for row in file_data]
+
+        # Check and fill missing values with "None"
+        for row in file_data_with_filename:
+            while len(row) < 13:
+                row.append(None)
+
+        # Append the data to the list
+        all_data.extend(file_data_with_filename)
+
+# Now 'all_data' contains the data from all CSV files with the filename as the first column
 for i in range(10):
     print(all_data[i])
--- model/preprocess.py
+++ model/preprocess.py
@@ -1,1 +1,5 @@
-from load_xlsx import data as xlsx_data
+# from load_xlsx import data as xlsx_data
+# from load_ångan import data as ångan_data
+from load_dans import all_data as dans_data
+
+print(len(dans_data))
