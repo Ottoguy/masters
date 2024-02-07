061c60d73910a3bfb5d4eab6191975bc8984dcee
Ottoguy <otto.palmlof@gmail.com>
2024-02-07 17:49:40 +0100

    Update famd.py

--- model/famd/famd.py
+++ model/famd/famd.py
@@ -15,7 +15,6 @@ sys.path.insert(0, parentdir)
 import functions
 from functions import export_csv_for_id
 
-
 warnings.filterwarnings("ignore")
 
 # Specify the directory where your files are located
@@ -43,29 +42,26 @@ data = pd.read_csv(latest_data_file)
 # Load meta data from the latest meta file
 meta_data = pd.read_csv(latest_meta_file)
 
+# Merge data and meta data on the common ID column
+merged_data = pd.merge(data, meta_data, on='ID')
+
+# Drop columns that are not needed
+merged_data = merged_data.drop(columns=['Timestamp', 'Filename'], axis=1)
+
+# Group by 'ID' and aggregate numerical and categorical data as lists
+grouped_data = merged_data.groupby('ID').agg(lambda x: x.tolist())
+
 # Separate numerical and categorical columns
 numerical_cols = ['Timestamp_sin', 'Timestamp_cos', 'Phase1Current', 'Phase2Current', 'Phase3Current',
                   'Phase1Voltage', 'Phase2Voltage', 'Phase3Voltage']
-categorical_cols = ['Effect', 'ChargingStatus']
+categorical_cols = ['ChargingStatus']
 
 # Extract required numerical and categorical features from the meta data
 numerical_meta_cols = ['Half_Minutes', 'Charging_Half_Minutes', 'TimeConnected_sin', 'TimeConnected_cos', 
                        'TimeDisconnected_sin', 'TimeDisconnected_cos', 'Energy_Uptake']
 categorical_meta_cols = ['FullyCharged', 'ChargingPoint', 'Current_Type', 'Weekend']
-
-# Merge data and meta data on the common ID column
-merged_data = pd.merge(data, meta_data[['ID'] + numerical_meta_cols + categorical_meta_cols], on='ID')
-
-# Drop columns that are not needed
-merged_data = merged_data.drop(columns=['Timestamp', 'Filename'], axis=1)
-
-# Separate numerical and categorical columns
-numerical_cols = numerical_cols + numerical_meta_cols
-categorical_cols = categorical_cols + categorical_meta_cols
-
-
-print("Performing FAMD...")
-# Perform Factor Analysis of Mixed Data (FAMD)
+print(grouped_data[categorical_cols])
+# Apply FAMD on grouped data
 famd = FAMD(
     n_components=2,
     n_iter=3,
@@ -73,16 +69,17 @@ famd = FAMD(
     check_input=True,
     random_state=42,
     engine="sklearn",
-    handle_unknown="error"  # same parameter as sklearn.preprocessing.OneHotEncoder
+    handle_unknown="error"
 )
 print("Fitting FAMD...")
-famd.fit(merged_data[numerical_cols + categorical_cols])
+famd.fit(grouped_data[numerical_cols + categorical_cols + numerical_meta_cols + categorical_meta_cols])
 
+# Print FAMD results
 print("FAMD Results:")
 print("Eigenvalues:")
 print(famd.eigenvalues_summary)
 print("Row coordinates:")
-print(famd.row_coordinates(merged_data).head())
+print(famd.row_coordinates(grouped_data).head())
 print("Column coordinates:")
 print(famd.column_coordinates_)
 print("Row contributions:")
@@ -97,20 +94,12 @@ print(famd.column_contributions_.style.format('{:.0%}'))
 
 # Plot
 famd.plot(
-    merged_data,
+    grouped_data,
     x_component=0,
     y_component=1
 )
 
-print("Principal components:")
 # Get the principal components
-principal_components = famd.transform(merged_data[numerical_cols + categorical_cols])
+principal_components = famd.transform(grouped_data[numerical_cols + categorical_cols])
 
 export_csv_for_id(principal_components, "famd")
-
-# Scatter plot of principal components
-plt.scatter(principal_components.iloc[:, 0], principal_components.iloc[:, 1])
-plt.title('Scatter Plot of Principal Components')
-plt.xlabel('Principal Component 1')
-plt.ylabel('Principal Component 2')
-plt.show()
